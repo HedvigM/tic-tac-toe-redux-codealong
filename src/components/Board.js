ba7aa0dd5fab330d376ceff64c172028ca0fb4bd
HedvigM <hedvig@mejstedt.se>
2021-11-07 15:53:56 +0100

    coded along

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Square } from 'components/Square'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Square } from 'components/Square';
 
 export const Board = () => {
   // TODO - use the 'squares' state from the game reducer
-  const squares = []
+  // Store innuto useSelector tar hela store och seden väljer vi vad vi skall använda av store i det som kommer efter där.
+  const squares = useSelector((store) => store.game.squares);
 
   return (
-    <div className="board">
+    <div className='board'>
       {squares.map((value, index) => (
         <Square key={index} value={value} index={index} />
       ))}
     </div>
-  )
-}
+  );
+};
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Player } from 'components/Player'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { game } from 'reducers/game';
+import { Player } from 'components/Player';
 
 export const Square = ({ value, index }) => {
+  const dispatch = useDispatch();
   const handleClick = () => {
+    dispatch(game.actions.captureSquare({ index }));
     // TODO send the captureSquare action
-  }
+  };
 
   return (
     <button
       className={value === null ? 'square clickable' : 'square'}
-      type="button"
-      onClick={handleClick}>
+      type='button'
+      onClick={handleClick}
+    >
       <Player value={value} />
     </button>
-  )
-}
+  );
+};
